26fe9c107a340e75a143f9c87397dbf429dc2ad3
maustem <diego.maustem@gmail.com>
2025-02-12 00:14:58 -0300

    :sparkles: feat: Creating first tests of registrations

--- tests/Feature/RegistrationControllerTest.php
+++ tests/Feature/RegistrationControllerTest.php
@@ -4,10 +4,25 @@ namespace Tests\Feature;
 
 use App\Models\Registration;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RegistrationControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function test_destroy_deletes_registration()
+    {
+        $registration = Registration::factory()->create();
+
+        $response = $this->deleteJson("/libro/registrations/{$registration->id}");
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'message' => 'Excluded registration.',
+        ]);
+        
+        $this->assertDatabaseMissing('registrations', ['id' => $registration->id]);
+    }
+ 
 
 }
