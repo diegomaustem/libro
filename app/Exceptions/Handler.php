eac5526276ab3b7db8c9c248a7255003f45d1733
diegomaustem <diego.maustem@gmail.com>
2025-04-30 16:16:49 -0300

    :recycle: refactor: Adding API handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +27,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException && $request->expectsJson()) {
+            return response()->json([
+                'message' => "Record with id {$e->getIds()[0]} not found.",
+                'resource' => class_basename($e->getModel()),
+            ], 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
